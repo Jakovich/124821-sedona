30fef5fa4a1930d0b75092146836a3d225842dc0
Nadia Pillard <ua9ond@yandex.ru>
2016-03-19 13:59:22 +0600

    main nav open/close done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector(".main-nav__wrapper");
+var menuClose = menu.querySelector(".main-nav__close");
+var menuToogle = document.querySelector(".main-nav__toogle");
+
+menuToogle.addEventListener("click", function (event) {
+  event.preventDefault;
+  menu.classList.remove("main-nav__wrapper--closed");
+});
+
+menuClose.addEventListener("click", function (){
+  menu.classList.add("main-nav__wrapper--closed");
+});
