1e4fb028a1a340a7847844f4d05e08403e53434f
Nadia Pillard <ua9ond@yandex.ru>
2016-04-03 11:54:04 +0700

    Some bugs fixes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-var menu = document.querySelector(".main-nav__wrapper");
-var menuClose = menu.querySelector(".main-nav__close");
-var menuToogle = document.querySelector(".main-nav__toogle");
+(function() {
+  var menu = document.querySelector(".main-nav__wrapper");
+  var menuClose = menu.querySelector(".main-nav__close");
+  var menuToogle = document.querySelector(".main-nav__toogle");
 
-onload = function() {
-  menu.classList.add("main-nav__wrapper--closed");
-};
+  onload = function() {
+    menu.classList.add("main-nav__wrapper--closed");
+  };
 
-menuToogle.addEventListener("click", function (event) {
-  event.preventDefault;
-  menu.classList.remove("main-nav__wrapper--closed");
-});
+  menuToogle.addEventListener("click", function (event) {
+    event.preventDefault;
+    menu.classList.remove("main-nav__wrapper--closed");
+  });
 
-menuClose.addEventListener("click", function (){
-  menu.classList.add("main-nav__wrapper--closed");
-});
+  menuClose.addEventListener("click", function (){
+    menu.classList.add("main-nav__wrapper--closed");
+  });
+  
+})();
